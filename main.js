b1b7f4f7c7a60be585828c31b09627ad78b0e072
adnan <adnankhan.cs@gmail.com>
2022-12-12 11:08:37 -0800

    third commit

--- main.js
+++ main.js
@@ -2,22 +2,32 @@ const div=document.querySelector("#btn");
 
 div.style.background='brown';
 div.addEventListener('click', a);
+
+
+
+
+    // console.log(cat);
+    // console.log(b[i].innerText+'---'+a[i].value);
+
+
+
+
 function a(e){
     e.preventDefault(); 
     //console.log(1);
     const a=document.querySelectorAll('input');
     const b=document.querySelectorAll('label');
-    // var cat={}
+    var cat={}
     for(var i=0;i<a.length-1;i++){
-        // cat[b[i].innerText]=a[i].value;
-        localStorage.setItem(b[i].innerText,a[i].value)
+        cat[b[i].innerText]=a[i].value;
+
         // console.log(cat);
         // console.log(b[i].innerText+'---'+a[i].value);
 
     
 
     }
-    
+    localStorage.setItem(a[1].value,cat)
     console.log(cat);
 
 }
